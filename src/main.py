f79aab278730636d7af8211865e7cbe63fc3b41d
marioax <santiagoadris171@gmail.com>
2023-09-09 02:50:27 -0300

    removing datadog

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 async def not_impl():
-	return {msg: "Not yet implemented"} 
+	return {"message": "Not yet implemented"} 
+
+
+@app.get("/")
+async def root():
+	return {"message": "Users microsevice"}
 
 app.get("/users")(not_impl)
 app.post("/users")(not_impl)
