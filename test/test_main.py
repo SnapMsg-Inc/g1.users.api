d1a1a83fa0b0856cc32a489731e7fc5a04902d6c
marioax <santiagoadris171@gmail.com>
2023-09-17 17:29:06 -0300

    adding tests with in-memory db

--- test/test_main.py
+++ test/test_main.py
@@ -1,14 +1,64 @@
+import pytest
 from fastapi.testclient import TestClient
-from src.main import app  
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool 
 
-client = TestClient(app)
+from src.main import app, get_db
+from src.models import User, UserCreate, UserRead, UserUpdate, Follow
+from src.database import USERS_DB_URL, connect_args
 
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Users microsevice"}
 
-def test_get_users():
-    response = client.get("/users")
-    assert response.status_code == 200
-    assert response.json() == {"message": "get users"}
+'''
+	Fixtures
+'''
+@pytest.fixture(name="db")
+def db_fixture():
+	# use in-memory database
+	engine = create_engine("sqlite://", connect_args=connect_args, poolclass=StaticPool)
+	SQLModel.metadata.create_all(engine)
+	with Session(engine) as db: 
+		yield db
+
+@pytest.fixture(name="client")
+def client_fixture(db: Session):
+	# override `get_db` dependecy
+	def get_db_override():
+		return db
+
+	app.dependency_overrides[get_db] = get_db_override
+	client = TestClient(app)
+	yield client
+	app.dependency_overrides.clear()
+
+
+'''
+	Users API Tests
+'''
+
+def test_root(client: TestClient):
+	response = client.get("/")
+	assert response.status_code == 200
+	assert response.json() == {"message": "users microsevice"}
+
+
+def test_get_users_one_user(db: Session, client: TestClient):
+	db_user = User(
+		uid=0,
+		email="example@example.com",
+		fullname="John Doe",
+		nick="eljuancho",
+		zone="U.S."
+	)
+	db.add(db_user)
+	db.commit()
+
+	response = client.get("/users")
+	users = response.json()
+
+	assert response.status_code == 200
+
+	for user in users:
+		assert user == db_user # ORM magic
+
+
+
