613164fd9098f148aefbe996f9b907a31620c756
marioax <santiagoadris171@gmail.com>
2023-09-18 16:37:38 -0300

    formatting code

--- src/database.py
+++ src/database.py
@@ -1,9 +1,9 @@
 from sqlmodel import SQLModel, create_engine
-from . import models 
+from . import models
 
 USERS_DB_URL = "postgresql://snapmsg:1234@users-db:5432/usersdb"
 engine = create_engine(USERS_DB_URL, echo=True)
 
-def init_tables():
-	SQLModel.metadata.create_all(engine)
 
+def init_tables():
+    SQLModel.metadata.create_all(engine)
--- src/models.py
+++ src/models.py
@@ -2,46 +2,43 @@ from sqlmodel import Field, SQLModel, Column, String, JSON, ARRAY
 from typing import Set, List, Optional
 
 
-class User(SQLModel, table=True): 
-	__tablename__ = "users"	
+class User(SQLModel, table=True):
+    __tablename__ = "users"
 
-	uid: str = Field(default=None, primary_key=True)
-	email: str
-	fullname: str
-	nick: str
-	interests: List[str] = Field(default=[], sa_column=Column(JSON))
-	#followers: Set[str] = Field(default=None , sa_column=Column(ARRAY(String())))
-	#followings: Set[str] = Field(default=None , sa_column=Column(ARRAY(String())))
-	zone: str
-	is_admin: bool = False
-
-
-class UserCreate(SQLModel): 
-	email: str
-	fullname: str
-	nick: str
-	interests: List[str] = Field(default=None, sa_column=Column(JSON))
-	zone: str
+    uid: str = Field(default=None, primary_key=True)
+    email: str
+    fullname: str
+    nick: str
+    interests: List[str] = Field(default=[], sa_column=Column(JSON))
+    #followers: Set[str] = Field(default=None , sa_column=Column(ARRAY(String())))
+    #followings: Set[str] = Field(default=None , sa_column=Column(ARRAY(String())))
+    zone: str
+    is_admin: bool = False
 
 
-class UserRead(SQLModel): 
-	uid: Optional[str] = None
-	email: Optional[str] = None
-	nick: Optional[str] = None
+class UserCreate(SQLModel):
+    email: str
+    fullname: str
+    nick: str
+    interests: List[str] = Field(default=None, sa_column=Column(JSON))
+    zone: str
 
 
-class UserUpdate(SQLModel): 
-	nick: str
-	interests: List[str] = Field(default=None, sa_column=Column(JSON))
+class UserRead(SQLModel):
+    uid: Optional[str] = None
+    email: Optional[str] = None
+    nick: Optional[str] = None
 
+
+class UserUpdate(SQLModel):
+    nick: str
+    interests: List[str] = Field(default=None, sa_column=Column(JSON))
 
 
 class Follow(SQLModel, table=True):
-	__tablename__ = "follows"
+    __tablename__ = "follows"
 
-	# {uid} follows {followed_uid}
-	fid: Optional[int] = Field(default=None, primary_key=True)
-	uid: str
-	followed_uid: str
-
-
+    # {uid} follows {followed_uid}
+    fid: Optional[int] = Field(default=None, primary_key=True)
+    uid: str
+    followed_uid: str
