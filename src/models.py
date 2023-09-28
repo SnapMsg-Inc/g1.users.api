224e27bb7f2387b74667681a4142e503ecdf2f3e
marioax <santiagoadris171@gmail.com>
2023-09-28 15:14:48 -0300

    Revert "Merge branch 'feature_follows' into dev"
    This reverts commit 97eb2476fd87210f48f332659d01aaa08de5a808, reversing
    changes made to 2632259885e42c5c54d66c3d25714031e432414f.

--- src/models.py
+++ src/models.py
@@ -1,54 +1,48 @@
-from sqlmodel import Field, SQLModel, Column, String, JSON, ARRAY, ForeignKey, PrimaryKeyConstraint
+from sqlmodel import Field, SQLModel, Column, String, JSON, ARRAY
 from typing import Set, List, Optional
 from datetime import date
 
 
 class User(SQLModel, table=True): 
 	__tablename__ = "users"	
-	
+
 	uid: str = Field(default=None, primary_key=True)
 	email: str
 	fullname: str
 	nick: str
-	birthdate: date
-	interests: Optional[List[str]] = Field(default=[], sa_column=Column(JSON), nullable=True)
-	zone: Optional[str] = Field(default=None, nullable=True)
+	interests: List[str] = Field(default=[], sa_column=Column(JSON))
+	#followers: Set[str] = Field(default=None , sa_column=Column(ARRAY(String())))
+	#followings: Set[str] = Field(default=None , sa_column=Column(ARRAY(String())))
+	zone: str
 	is_admin: bool = False
-	description: Optional[str] = Field(default=None, nullable=True)
-	ocupation: Optional[str] = Field(default=None, nullable=True)
 
 
-	
 class UserCreate(SQLModel): 
 	email: str
 	fullname: str
 	nick: str
-	interests: Optional[List[str]]= Field(default=None, sa_column=Column(JSON))
-	zone: Optional[str] = Field(default=None, nullable=True)
-	birthdate: date
-	description: Optional[str] = Field(default=None, nullable=True)
-	ocupation: Optional[str] = Field(default=None, nullable=True) 
+	interests: List[str] = Field(default=None, sa_column=Column(JSON))
+	zone: str
 
 
 class UserRead(SQLModel): 
 	uid: Optional[str] = None
 	email: Optional[str] = None
 	nick: Optional[str] = None
-	description: Optional[str] = Field(default=None, nullable=True)
-	ocupation: Optional[str] = Field(default=None, nullable=True)
 
 
 class UserUpdate(SQLModel): 
 	nick: str
-	zone: str
-	interests: List[str] = Field(sa_column=Column(JSON))
-	description: str
-	ocupation: str
+	interests: List[str] = Field(default=None, sa_column=Column(JSON))
 
-class Follow(SQLModel, table=True):	
+
+
+class Follow(SQLModel, table=True):
 	__tablename__ = "follows"
-	__table_args__ = (PrimaryKeyConstraint('uid', 'followed'),)
-		
-	uid: str = Field(sa_column=Column(String, ForeignKey('users.uid', ondelete='CASCADE')))
-	followed: str = Field(sa_column=Column(String, ForeignKey('users.uid', ondelete='CASCADE')))
-	
+
+	# {uid} follows {followed_uid}
+	fid: Optional[int] = Field(default=None, primary_key=True)
+	uid: str
+	followed_uid: str
+
+
